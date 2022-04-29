4435f661889a7e1ff9a49a826fa14eccac28daf8
Teixeira <claudioteixeira@outlook.com>
2022-04-29 16:00:58 -0300

    Primeiro Commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const produtos = require("./data/produtos");
+const marcas = require("./data/marcas");
+const categorias = require("./data/categorias");
+const tipos = require("./data/tipos")
+
+module.exports = {
+  produtos,
+  marcas,
+  categorias,
+  tipos
+}
